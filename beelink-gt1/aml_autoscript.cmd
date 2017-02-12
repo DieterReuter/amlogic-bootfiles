15cce97eef32815dcbf88f7ef21ad5ca0c4df3ce
Dieter Reuter <dieter.reuter@me.com>
2017-02-12 13:01:21 +0000

    Break boot when no kernel files are present

--- beelink-gt1/aml_autoscript.cmd
+++ beelink-gt1/aml_autoscript.cmd
@@ -2,7 +2,7 @@ defenv
 setenv bootdelay 5
 setenv bootfromnand 0
 setenv bootfromrecovery 0
-setenv bootcmd "run start_autoscript;run storeboot;"
+setenv bootcmd "run start_autoscript;"
 setenv start_autoscript "if usb start ; then run start_usb_autoscript;fi;if mmcinfo; then run start_mmc_autoscript;fi;"
 setenv start_mmc_autoscript "if fatload mmc 0 1020000 s905_autoscript; then autoscr 1020000; fi"
 setenv start_usb_autoscript "if fatload usb 0 1020000 s905_autoscript; then autoscr 1020000; fi"
